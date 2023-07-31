de0e31518e818f2192c8c040f9cc20ed2b0fcfa9
Zack Harris <zack.idea@gmail.com>
2023-07-31 12:06:41 -0400

    WPF now working with basic UI with button and textbox

---
+++ AudioCapturePlugin/PluginView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AudioCapturePlugin
+{
+	/// <summary>
+	/// Interaction logic for PluginView.xaml
+	/// </summary>
+	public partial class PluginView : UserControl
+	{
+		public PluginView()
+		{
+			InitializeComponent();
+		}
+
+		private void SaveFile_Button_Click(object sender, RoutedEventArgs e)
+		{
+			SaveFileDialog dlg = new SaveFileDialog();
+			if (dlg.ShowDialog() == true)
+			{
+				Debug.WriteLine("ZZZ: Filename: " + dlg.FileName + "   File Duration: " + Duration_TextBox.Text);
+			}
+		}
+    }
+}
